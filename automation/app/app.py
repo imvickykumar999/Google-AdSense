190d54f8524f330ddbbba47ad50996aa075473e4
imvickykumar999 <imvickykumar999@gmail.com>
2023-12-13 16:10:30 +0530

    login register

--- automation/app/app.py
+++ automation/app/app.py
@@ -1,88 +1,52 @@
 
 import requests, random
-from flask import Flask, render_template, send_from_directory
-from bs4 import BeautifulSoup as bs
+from flask_sqlalchemy import SQLAlchemy
 
-from newsapi import NewsApiClient
-newsapi = NewsApiClient(api_key='39e270768fef4cfe848af36d98107e82')
-sources = newsapi.get_sources()['sources']
+from flask import (Flask, 
+    render_template, 
+    redirect, 
+    url_for, 
+    request
+)
 
 app = Flask(__name__)
+app.app_context().push()
 
-@app.route('/<filename>')
-def ads(filename):
-    if '.' in filename:
-        filename = 'ads.txt'
-    return send_from_directory("", filename)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
-def get_news(source):
-    if source == 'inshorts':
+class user(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(80))
 
-        link = 'https://inshorts.com/en/read'
-        req = requests.get(link)
+def get_news(source, bot_token='982e523813b347c6a6404fd1f9b6e41c'):
+    try:
+        bot_token = '982e523813b347c6a6404fd1f9b6e41c'
+        gets = f'https://newsapi.org/v1/articles?source={source}&sortBy=top&apiKey={bot_token}'
+        
+        req = requests.get(gets) 
+        box = req.json()['articles']
 
-        soup = bs(req.content, 'html5lib')
-        box = soup.findAll('div', attrs = {'class':'news-card z-depth-1'})
+    except Exception as e:
+        print(e)
 
-        ha,ia,ba,la = [],[],[],[]
-        for i in range(len(box)):
-            h = box[i].find('span', attrs = {'itemprop':'headline'}).text
+    ha,ia,ba,la = [],[],[],[]
 
-            m = box[i].find('div', attrs = {'class':'news-card-image'})
-            m = m['style'].split("'")[1]
+    for i in range(len(box)):
+        h = box[i]['title']
+        m = box[i]['urlToImage']
+        b = box[i]['description']
 
-            b = box[i].find('div', attrs = {'itemprop':'articleBody'}).text
-            l = 'link not found'
+        try: l = box[i]['url']
+        except: l = 'link not found'
 
-            try:
-                l = box[i].find('a', attrs = {'class':'source'})['href']
-            except:
-                pass
-            
-            ha.append(h)
-            ia.append(m)
-            ba.append(b)
-            la.append(l)
+        ha.append(h)
+        ia.append(m)
+        ba.append(b)
+        la.append(l)
 
-    else:
-        try:
-            bot_token = '39e270768fef4cfe848af36d98107e82'
-            gets = f'https://newsapi.org/v1/articles?source={source}&sortBy=top&apiKey={bot_token}'
-            
-            req = requests.get(gets) 
-            box = req.json()['articles']
-
-        except:
-            try:
-                bot_token = 'e1b57251b1b94ed894f3c60d25551eb2'
-                gets = f'https://newsapi.org/v1/articles?source={source}&sortBy=top&apiKey={bot_token}'
-                
-                req = requests.get(gets) 
-                box = req.json()['articles']
-
-            except:
-                bot_token = '5f69434d32434ea8bdb16b347f71cca2'
-                gets = f'https://newsapi.org/v1/articles?source={source}&sortBy=top&apiKey={bot_token}'
-                
-                req = requests.get(gets) 
-                box = req.json()['articles']
-
-        ha,ia,ba,la = [],[],[],[]
-        for i in range(len(box)):
-            h = box[i]['title']
-            m = box[i]['urlToImage']
-            b = box[i]['description']
-            l = 'link not found'
-
-            try:
-                l = box[i]['url']
-            except:
-                pass
-
-            ha.append(h)
-            ia.append(m)
-            ba.append(b)
-            la.append(l)
     return ha, ia, ba, la
 
 @app.route('/news/<source>')
@@ -93,40 +57,81 @@ def one_news(source):
                                 ha=ha, 
                                 ia=ia, 
                                 ba=ba, 
-                                la=la, 
-                                sources=sources,
+                                la=la,
                                 len = len(ha))
     except:
-        return render_template('404.html')
+        return render_template('home.html', api_key=False)
 
-@app.route('/home')
+@app.route('/home', methods=['POST', 'GET'])
 def home():
-    return render_template('home.html')
+    try:
+        if request.method == 'POST':
+            api_key = request.form['api_key']
+            source = ['bbc-news', 'cnn', 'the-verge', 'time', 'the-wall-street-journal']
+            source = random.choice(source)
+
+            ha, ia, ba, la = get_news(source, api_key)
+            return render_template('news.html', 
+                                    ha=ha, 
+                                    ia=ia, 
+                                    ba=ba, 
+                                    la=la,
+                                    len = len(ha))
+        else:
+            return render_template('home.html', api_key=True)
+    except: return render_template('404.html')
 
 @app.route('/')
 def news():
     source = ['bbc-news', 'cnn', 'the-verge', 'time', 'the-wall-street-journal']
-    
-    # https://newsapi.org/account
     source = random.choice(source)
-    print(source)
 
-    ha, ia, ba, la = get_news(source)
-    # ha, ia, ba, la = get_news('inshorts')
-
-    return render_template('news.html', 
-                            ha=ha, 
-                            ia=ia, 
-                            ba=ba, 
-                            la=la, 
-                            sources=sources,
-                            len = len(ha))
+    try:
+        ha, ia, ba, la = get_news(source)
+        return render_template('news.html', 
+                                ha=ha, 
+                                ia=ia, 
+                                ba=ba, 
+                                la=la,
+                                len = len(ha))
+    except:
+        return render_template('home.html', api_key=False)
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+@app.route("/welcome/<uname>")
+def welcome(uname):
+    return render_template("welcome.html", uname=uname)
+
+@app.route("/login",methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        uname = request.form["uname"]
+        passw = request.form["passw"]
+        
+        login = user.query.filter_by(username=uname, password=passw).first()
+        if login is not None:
+            return redirect(url_for("welcome", uname=uname))
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        uname = request.form['uname']
+        mail = request.form['mail']
+        passw = request.form['passw']
+
+        register = user(username = uname, email = mail, password = passw)
+        db.session.add(register)
+        db.session.commit()
+
+        return redirect(url_for("login"))
+    return render_template("register.html")
+
 if __name__ == '__main__':
+    db.create_all()
     app.run(
         # host="0.0.0.0", 
         debug=True
